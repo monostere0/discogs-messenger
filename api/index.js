232534a3a02a7877fc0a4ecc7c059b294fc2ad71
Daniel Leu <leudanielm@gmail.com>
2019-02-21 19:36:10 +0100

    Use SSL for Koa

--- api/index.js
+++ api/index.js
@@ -1,6 +1,14 @@
 const conf = require('./conf')();
 const app = require('./app');
+const https = require('https');
+const fs = require('fs');
 
-app.listen(process.env.PORT || conf.port /* First Heroku, then our conf */,
-  // eslint-disable-next-line no-console
-  () => console.log(`Koa is listening on localhost:${conf.port}`));
+const httpsOptions = {
+  key: fs.readFileSync('./certs/key.pem'),
+  cert: fs.readFileSync('./certs/cert.pem'),
+};
+
+https.createServer(
+  httpsOptions,
+  app.callback(),
+).listen(process.env.port || conf.port);
