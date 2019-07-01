bb9716481b02ed5996709598a7de690e52869f0e
Daniel Leu <leudanielm@gmail.com>
2019-07-02 00:20:17 +0200

    Add docker script, update running instructions

--- api/terraform/shared/vars.tf
+++ api/terraform/shared/vars.tf
@@ -1,4 +1,6 @@
-variable "aws_region" {}
+variable "aws_region" {
+  default = "eu-central-1"
+}
 variable "aws_access_key_id" {}
 variable "aws_secret_access_key" {}
 
@@ -7,3 +9,4 @@ variable "aws_zones" {
   description = "List of availability zones in use"
   default     = ["eu-central-1", "eu-west-2"]
 }
+
